0b4be58a458ac4f29a4995ddc6c5c6a8e6ed9188
Kelsie <kelsie51@hotmail.com>
2021-05-17 00:31:26 -0500

    style changes to jumbotron, added image and css

--- client/src/components/Jumbotron/index.js
+++ client/src/components/Jumbotron/index.js
@@ -1,11 +1,34 @@
 import React from "react";
+import "./index.css";
+import bookPhoto from './bookPhoto.JPG';
 
 function Jumbotron({ children }) {
   return (
     <div
-      style={{ height: 300, clear: "both", paddingTop: 120, textAlign: "center" }}
+      style={{ 
+        height: 300, 
+        fontSize: "1em",
+        clear: "both",
+         paddingTop: 125,
+          textAlign: "center",
+          backgroundImage: `url(${bookPhoto})`,
+          backgroundSize: "cover"
+      }}
       className="jumbotron"
     >
+      <h1 className="header">
+        <span style={{ color: '#4285F4' }} >B</span>
+        <span style={{ color: '#DB4437' }} >o</span>
+        <span style={{ color: '#F4B400' }} >o</span>
+        <span style={{ color: '#4285F4' }} >K</span>
+
+        <span style={{ color: '#0F9D58' }} >L</span>
+        <span style={{ color: '#DB4437' }} >o</span>
+        <span style={{ color: '#4285F4' }} >o</span>
+        <span style={{ color: '#DB4437' }} >k</span>
+        <span style={{ color: '#F4B400' }} >e</span>
+        <span style={{ color: '#4285F4' }} >r</span>
+      </h1>
       {children}
     </div>
   );
